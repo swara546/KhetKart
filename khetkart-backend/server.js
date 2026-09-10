bf9c2a3f7b1f5d9a2c5e99a862a53ea00dfa9d63
Swara <swaranatekar546@gmail.com>
2026-09-10 11:47:39 +0530

    cors changes

--- khetkart-backend/server.js
+++ khetkart-backend/server.js
@@ -10,7 +10,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://khet-kart.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
